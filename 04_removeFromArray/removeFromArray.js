9993f4cabd6716133af9054d1289a87d87190f7d
Persephone <134444980+PersephoneXO@users.noreply.github.com>
2023-11-17 13:53:49 -0500

    01-04 finished

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...nums) {
+    for (const each of nums){
+        if (arr.includes(each)){
+            let index=arr.indexOf(each);
+            arr.splice(index,1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
