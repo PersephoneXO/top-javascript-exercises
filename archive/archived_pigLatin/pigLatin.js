fd0bc7e91da9bcb469533793fc7baecce42394e4
Persephone <134444980+PersephoneXO@users.noreply.github.com>
2023-11-30 10:52:44 -0500

    archive-pig latin finished

--- archive/archived_pigLatin/pigLatin.js
+++ archive/archived_pigLatin/pigLatin.js
@@ -1,6 +1,36 @@
-function pigLatin(string) {
+function pigLatin(str) {
+    let strSplit=str.split(' ');
+    let newStr=[];
+    for(const word of strSplit){
 
+    let vowels='aeio';
+    let newWord='';
+    if(vowels.includes(word[0])){
+        newWord+=word+'ay';
+    }
+    else{
+        let firstVowel=false;
+        let vowelIndex=0;
+        while(!firstVowel){
+            vowelIndex++;
+            if(vowels.includes(word[vowelIndex])){
+                firstVowel=true;
+            }
+        }
+
+        let newStart=word.substring(vowelIndex);
+        newWord+=newStart;
+
+        for(let i=0;i<vowelIndex;i++){
+            newWord+=word[i];
+        }
+        newWord+='ay';
+
+    }
+    newStr.push(newWord);
+}
+    return newStr.join(' ');
 };
-  
+
 // Do not edit below this line
 module.exports = pigLatin;
